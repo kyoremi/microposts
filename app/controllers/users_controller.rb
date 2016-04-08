ea9e78e76139824e15070c158bb3840e6df39697
kyoremi <kyoshiro.yamashita@gmail.com>
2016-04-08 01:29:58 +0000

    before submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
+  
   def show 
    @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,11 +17,20 @@ class UsersController < ApplicationController
     render 'new'
     end
   end
-
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile edit"
+      redirect_to edit_user_path
+    end
+  end
+  
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :prefecture_id, :url, :self_introduction)
   end
 end
