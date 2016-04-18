fa0409cdb353cc6e5c1e67b6f1b77d47e70ac933
kyoremi <kyoshiro.yamashita@gmail.com>
2016-04-18 01:33:38 +0000

    1commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users=User.all
+  end
+  
   def show 
    @user = User.find(params[:id])
    @microposts = @user.microposts.order(created_at: :desc)
