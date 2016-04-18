3f4cb98ffb78044baa48ffc17caf1b27a373aece
kyoremi <kyoshiro.yamashita@gmail.com>
2016-04-18 03:08:47 +0000

    submit418

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,14 @@
 class StaticPagesController < ApplicationController
-   before_action :logged_in_user
+   before_action :signup
    
  def home
  end
+ 
+ private
+  def signup
+    unless logged_in?
+      store_location
+      redirect_to signup_path
+    end 
+  end
 end
